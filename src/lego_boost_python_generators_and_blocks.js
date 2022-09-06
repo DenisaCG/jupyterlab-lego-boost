10b4dfc4bce8f447dd19419ff11839e4630ad764
DenisaCG <denisa.checiu@gmail.com>
2022-09-06 15:09:37 +0200

    added connection block for vernie

--- src/lego_boost_python_generators_and_blocks.js
+++ src/lego_boost_python_generators_and_blocks.js
@@ -26,12 +26,15 @@ SOFTWARE.
 
 import * as Blockly from 'blockly';
 import BlocklyPy from 'blockly/python';
+import Vernie from 'vernie.py';
 
 var lego_boost_color = '#550a8a';
 
 /*
  * Block definitions
  */
+
+// MoveHub
 
 Blockly.Blocks['lego_boost_connect'] = {
   init: function () {
@@ -194,9 +197,48 @@ Blockly.Blocks['lego_boost_sleep'] = {
     this.setHelpUrl('');
   }
 };
+
+// Vernie
+
+Blockly.Blocks['lego_boost_vernie_connect'] = {
+  init: function () {
+    // this.appendDummyInput().appendField('Connect to Vernie');
+    // this.setInputsInline(true);
+    // this.setPreviousStatement(true, null);
+    // this.setNextStatement(true, null);
+    // this.setColour(lego_boost_color);
+    // this.setTooltip('Connect to the Vernie robot.');
+    // this.setHelpUrl('');
+    this.appendValueInput('BLUETOOTH_ADDRESS')
+      .setCheck('String')
+      .appendField('Connect to Vernie on address');
+    // this.appendStatementInput('DO');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(lego_boost_color);
+    this.setTooltip('Connect to Vernie given its Bluetooth address.');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['lego_boost_vernie_say'] = {
+  init: function () {
+    this.appendValueInput('TEXT').setCheck('String').appendField('Vernie says');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(lego_boost_color);
+    this.setTooltip('Give Vernie a sentance to say.');
+    this.setHelpUrl('');
+  }
+};
+
 /*
  * Generators
  */
+
+// MoveHub
 
 Blockly.Python['lego_boost_connect'] = function (block) {
   var value_bluetooth_address = Blockly.Python.valueToCode(
@@ -385,11 +427,211 @@ Blockly.Python['lego_boost_sleep'] = function (block) {
   );
 
   var code = 'time.sleep(' + value_seconds + ')\n';
+  return code;
 };
 
 Blockly.Blocks['lego_boost_sleep'].toplevel_init = `
 import time
+
 `;
+
+// Vernie 
+
+Blockly.Python['lego_boost_vernie_connect'] = function (block) {
+  // var code = 'robot = Vernie(MoveHub)\n';
+  // return code;
+
+  var value_bluetooth_address = Blockly.Python.valueToCode(
+    block,
+    'BLUETOOTH_ADDRESS',
+    Blockly.Python.ORDER_ATOMIC
+  );
+
+  var code =
+    'conn = get_connection_bleak(hub_mac=' +
+    value_bluetooth_address +
+    ', hub_name=MoveHub.DEFAULT_NAME)\nrobot = Vernie(MoveHub(conn))\n';
+  return code;
+};
+
+Blockly.Blocks['lego_boost_vernie_connect'].toplevel_init = `
+# defining the Vernie class
+# coding=utf-8
+import hashlib
+import os
+import re
+import subprocess
+import time
+
+from pylgbst import *
+from pylgbst.hub import MoveHub
+
+try:
+    import gtts
+
+
+    def say(text):
+        print("%s" % text)
+        if isinstance(text, str):
+            text = text.decode("utf-8")
+        md5 = hashlib.md5(text.encode('utf-8')).hexdigest()
+        fname = "/tmp/%s.mp3" % md5
+        if not os.path.exists(fname):
+            myre = re.compile('[[A-Za-z]', re.UNICODE)
+            lang = 'en' if myre.match(text) else 'ru'
+
+            logging.getLogger('requests').setLevel(logging.getLogger('').getEffectiveLevel())
+            tts = gtts.gTTS(text=text, lang=lang, slow=False)
+            tts.save(fname)
+
+        with open(os.devnull, 'w') as fnull:
+            subprocess.call("mplayer %s" % fname, shell=True, stderr=fnull, stdout=fnull)
+except BaseException:
+    def say(text):
+        print("%s" % text)
+
+forward = FORWARD = right = RIGHT = 1
+backward = BACKWARD = left = LEFT = -1
+straight = STRAIGHT = 0
+
+SPEECH_LANG_MAP = {
+    'en': {
+        'ready': "Vernie the Robot is ready.",
+        "commands help": "Available commands are: "
+                         "forward, backward, turn left, turn right, "
+                         "head left, head right, head straight, shot and say",
+        "finished": "Thank you! Robot is now turning off",
+        "text is empty": "Please, enter not empty text to say!"
+    }
+}
+
+VERNIE_TO_MOTOR_DEGREES = 2.6
+VERNIE_SINGLE_MOVE = 430
+
+
+class Vernie(MoveHub):
+    def __init__(self, language='en'):
+        super(Vernie, self).__init__()
+        self.language = language
+
+        while True:
+            required_devices = (self.vision_sensor, self.motor_external)
+            if None not in required_devices:
+                break
+            log.debug("Waiting for required devices to appear: %s", required_devices)
+            time.sleep(1)
+
+        self._head_position = 0
+        self.motor_external.subscribe(self._external_motor_data)
+
+        self._reset_head()
+        self.say("ready")
+        time.sleep(1)
+
+    def say(self, phrase):
+        if phrase in SPEECH_LANG_MAP[self.language]:
+            phrase = SPEECH_LANG_MAP[self.language][phrase]
+        say(phrase)
+
+    def _external_motor_data(self, data):
+        log.debug("External motor position: %s", data)
+        self._head_position = data
+
+    def _reset_head(self):
+        self.motor_external.timed(1, -0.2)
+        self.head(RIGHT, angle=45)
+
+    def head(self, direction=RIGHT, angle=25, speed=0.1):
+        if direction == STRAIGHT:
+            angle = -self._head_position
+            direction = 1
+
+        self.motor_external.angled(direction * angle, speed)
+
+    def turn(self, direction, degrees=90, speed=0.3):
+        self.head(STRAIGHT, speed=0.5)
+        self.head(direction, 35, 1)
+        self.motor_AB.angled(int(VERNIE_TO_MOTOR_DEGREES * degrees), speed * direction, -speed * direction)
+        self.head(STRAIGHT, speed=0.5)
+
+    def move(self, direction, distance=1, speed=0.2):
+        self.head(STRAIGHT, speed=0.5)
+        self.motor_AB.angled(distance * VERNIE_SINGLE_MOVE, speed * direction, speed * direction)
+
+    def shot(self):
+        self.motor_external.timed(0.5)
+        self.head(STRAIGHT)
+        self.head(STRAIGHT)
+
+    def interpret_command(self, cmd, confirm):
+        cmd = cmd.strip().lower().split(' ')
+        if cmd[0] in ("head", "голова", "голову"):
+            if cmd[-1] in ("right", "вправо", "направо"):
+                confirm(cmd)
+                self.head(RIGHT)
+            elif cmd[-1] in ("left", "влево", "налево"):
+                confirm(cmd)
+                self.head(LEFT)
+            else:
+                confirm(cmd)
+                self.head(STRAIGHT)
+        elif cmd[0] in ("say", "скажи", "сказать"):
+            if not cmd[1:]:
+                self.say("text is empty")
+                return
+            say(' '.join(cmd[1:]))
+        elif cmd[0] in ("fire", "shot", "огонь", "выстрел"):
+            say("fire")
+            self.shot()
+        elif cmd[0] in ("end", "finish", "конец", "стоп"):
+            self.say("finished")
+            raise KeyboardInterrupt()
+        elif cmd[0] in ("forward", "вперёд", "вперед"):
+            try:
+                dist = int(cmd[-1])
+            except BaseException:
+                dist = 1
+            confirm(cmd)
+            self.move(FORWARD, distance=dist)
+        elif cmd[0] in ("backward", "назад"):
+            try:
+                dist = int(cmd[-1])
+            except BaseException:
+                dist = 1
+            confirm(cmd)
+            self.move(BACKWARD, distance=dist)
+        elif cmd[0] in ("turn", "поворот", 'повернуть'):
+            if cmd[-1] in ("right", "вправо", "направо"):
+                confirm(cmd)
+                self.turn(RIGHT)
+            elif cmd[-1] in ("left", "влево", "налево"):
+                confirm(cmd)
+                self.turn(LEFT)
+            else:
+                confirm(cmd)
+                self.turn(RIGHT, degrees=180)
+        elif cmd[0] in ("right", "вправо", "направо"):
+            confirm(cmd)
+            self.turn(RIGHT)
+        elif cmd[0] in ("left", "влево", "налево"):
+            confirm(cmd)
+            self.turn(LEFT)
+        elif cmd[0]:
+            self.say("Unknown command")
+            self.say("commands help")
+
+`;
+
+Blockly.Python['lego_boost_vernie_say'] = function (block) {
+  var value_text = Blockly.Python.valueToCode(
+    block,
+    'TEXT',
+    Blockly.Python.ORDER_ATOMIC
+  );
+
+  var code = 'robot.say(' + value_text + ')\n';
+  return code;
+};
 
 // Creating a toolbox containing all the main blocks
 // and adding the lego boost catgory.
@@ -792,6 +1034,14 @@ const TOOLBOX = {
         {
           kind: 'BLOCK',
           type: 'lego_boost_sleep'
+        },
+        {
+          kind: 'BLOCK',
+          type: 'lego_boost_vernie_connect'
+        },
+        {
+          kind: 'BLOCK',
+          type: 'lego_boost_vernie_say'
         }
       ]
     }
